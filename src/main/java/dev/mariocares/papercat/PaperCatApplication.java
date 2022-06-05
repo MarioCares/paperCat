c14e5acf053a7f7bdf8e8c43300ea9cac35f9d6e
Mario Cares <mario.cares.c@outlook.com>
2022-06-05 09:08:55 -0400

    Initial commit

---
+++ src/main/java/dev/mariocares/papercat/PaperCatApplication.java
@@ -0,0 +1,13 @@
+package dev.mariocares.papercat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperCatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaperCatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/mariocares/papercat/PaperCatApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mariocares.papercat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperCatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
